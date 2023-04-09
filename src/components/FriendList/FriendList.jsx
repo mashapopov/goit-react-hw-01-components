db0df3082d6714956ab05917b4272cf7d9c519f7
Maria Popova <0979867251m@gmail.com>
2023-04-10 00:33:34 +0300

    koment

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -5,6 +5,7 @@ import css from './Friend.module.css';
 const FriendList = ({ friends }) => {
   return (
     <ul className={css.friend__list}>
+      {/* коллекция */}
       {friends.map(({ avatar, name, isOnline, id }) => {
         return (
           <li key={id} className={css.item}>
